4ed0d526a3ba6e2b974e617f7f19b93ad36d09e5
ggkedo <34963190+ggkedo@users.noreply.github.com>
2024-07-19 19:22:43 +0200

    ShopCart class init

---
+++ service/ShopCart.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Service;
+
+class ShopCart
+{
+    public function getSize()
+    {
+        return 0;
+    }
+
+    public function isEmpty()
+    {
+        return true;
+    }
+
+    public function getTotal()
+    {
+        return 0;
+    }
+
+    public function add($item)
+    {
+
+    }
+
+    public function clear()
+    {
+        
+    }
+}
